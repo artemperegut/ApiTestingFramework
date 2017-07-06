8cacd8ea98351b3a6e0bcb460766f0868ce35ae5
Artem Peregut <artem.peregut@gmail.com>
2017-07-06 11:54:46 +0300

    Steps

--- src/main/java/steps/RequestSteps.java
+++ src/main/java/steps/RequestSteps.java
@@ -1,7 +1,16 @@
-package Steps;
+package steps;
+
+import cucumber.api.java.ru.Тогда;
+
+import static services.Request.request;
 
 /**
  * Created by user on 06.07.2017.
  */
 public class RequestSteps {
+
+    @Тогда("пользователь делает GET запрос в \"(.*?)\" и выводит ответ в консоль")
+    public void doRequest(String path) {
+        System.out.println(request.doGetAndReturnResponse(path, ""));
+    }
 }
