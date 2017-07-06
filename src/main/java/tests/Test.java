804d923ae48a16c78862c9b756e9c21be8e4294c
Artem Peregut <artem.peregut@gmail.com>
2017-07-06 11:54:29 +0300

    Test runner

--- src/main/java/tests/Test.java
+++ src/main/java/tests/Test.java
@@ -1,7 +1,14 @@
 package tests;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
 /**
  * Created by Artem Peregut on 06.07.2017.
  */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = {"src/main/java/steps"}, features = "src/main/resources/tests")
 public class Test {
 }
--- src/main/java/tests/Test1.java
+++ src/main/java/tests/Test1.java
@@ -1,7 +1,11 @@
 package tests;
 
+import cucumber.api.CucumberOptions;
+
 /**
  * Created by Artem Peregut on 06.07.2017.
  */
-public class Test1 {
+
+@CucumberOptions(tags = {"@test1"})
+public class Test1 extends Test {
 }
