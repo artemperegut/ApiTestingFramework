d65a242b2017ab38271589d7425f36dd7a348058
Artem Peregut <artem.peregut@gmail.com>
2017-07-06 12:33:51 +0300

    Steps fix

---
+++ src/main/java/steps/CommonSteps.java
@@ -0,0 +1,17 @@
+package steps;
+
+import cucumber.api.java.Before;
+import services.Request;
+
+/**
+ * Created by Artem Peregut on 06.07.2017.
+ */
+public class CommonSteps {
+    public static Request request;
+
+
+    @Before
+    public void startUp() {
+        request = new Request("https://httpbin.org");
+    }
+}
--- src/main/java/steps/RequestSteps.java
+++ src/main/java/steps/RequestSteps.java
@@ -2,15 +2,15 @@ package steps;
 
 import cucumber.api.java.ru.Тогда;
 
-import static services.Request.request;
+import static steps.CommonSteps.request;
 
 /**
- * Created by user on 06.07.2017.
+ * Created by Artem Peregut on 06.07.2017.
  */
 public class RequestSteps {
 
     @Тогда("пользователь делает GET запрос в \"(.*?)\" и выводит ответ в консоль")
     public void doRequest(String path) {
-        System.out.println(request.doGetAndReturnResponse(path, ""));
+        request.doGetAndReturnResponse(path, "application/json");
     }
 }
