5cd708819b33f845b620c65d9f828204f643d8d9
Artem Peregut <artem.peregut@gmail.com>
2017-07-10 15:23:40 +0300

    Added config file

--- src/test/java/steps/CommonSteps.java
+++ src/test/java/steps/CommonSteps.java
@@ -3,14 +3,22 @@ package steps;
 import cucumber.api.java.Before;
 import services.Request;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by Artem Peregut on 06.07.2017.
  */
 public class CommonSteps {
     public static Request request;
+    public static Properties prop;
 
     @Before
-    public void startUp() {
-        request = new Request("http://httpbin.org");
+    public void startUp() throws IOException {
+        prop = new Properties();
+        InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");
+        prop.load(input);
+        request = new Request(prop.getProperty("uri"));
     }
 }
